a9e14aed9b0908a59ec682809f6876f039c331f1
Rabbia Jahangir <rabbiajahangir92@gmail.com>
2015-10-16 17:54:11 +0500

    Base for multiplayer connectivity established, next step aplayer matching and android app integration

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 /**
  * Created by Jahangir on 10/8/2015.
  */
+/*jslint node:true*/
+"use strict";
 
-var app = require('express')();
-var server= require('http').Server(app)
-var io = require('socket.io')(server);
+var app = require('express')(),
+    server = require('http').Server(app),
+    io = require('socket.io')(server),
+    player = require('./player');
 
 server.listen(80);
 
@@ -14,16 +17,27 @@ app.get('/', function (req, res) {
 });
 var players = [];
 
-io.on('connection', function (player) {
-    console.log("Welcome");
-    players.push(player);
+function createPlayer(socket) {
+    players.push(socket);
     console.log(players.length);
-    console.log("player connected");
-    player.on('disconnect', function () {
-        players.splice(players.indexOf(player), 1);
-        console.log("player disconnected");
-        console.log(players.length);
+}
+function destroyPlayer(socket) {
+    for (var i = 0; i < players.length; i++) {
+        if (players[i].id == socket.id) {
+            players.splice(i, 1);
+        }
+    }
+    console.log(players.length);
+}
+
+io.on('connection', function (player) {
+    createPlayer(player);
+    console.log(player.id);
+
+    player.on('req', function (data) {
+        console.log(player.id + " " + data.msg);
     });
+
 
 });
 
---
+++ player.js
@@ -0,0 +1,7 @@
+/**
+ * Created by Jahangir on 10/16/2015.
+ */
+/*jslint node:true*/
+
+"use strict";
+
