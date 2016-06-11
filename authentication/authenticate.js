945b12fb8c3ad8c90bb5a99300ad4b88cc980659
Rabbia Umer <rabbiajahangir92@gmail.com>
2016-06-11 13:46:37 -0400

    passign the arguments needed, such as locla strategy

--- authentication/authenticate.js
+++ authentication/authenticate.js
@@ -2,68 +2,64 @@
  * Created by RabbiaUmer on 6/2/16.
  */
 
-module.exports = function (passport) {
-    console.log("inside authenticate.js");
+module.exports = function (passport, LocalStrategy) {
+
     /*----Authentication code starts here----*/
-    module.exports = function () {
 
-        passport.serializeUser(function (user, done) {
-            done(null, user.id);
+    passport.serializeUser(function (user, done) {
+        done(null, user.id);
+    });
+    passport.deserializeUser(function (id, done) {
+        user.findById(id, function (err, user) {
+            done(err, user);
         });
-        passport.deserializeUser(function (id, done) {
-            user.findById(id, function (err, user) {
-                done(err, user);
-            });
+    });
+
+    passport.use('signup-strategy', new LocalStrategy({
+        usernameField: 'email',
+        passwordField: 'password',
+        passReqToCallback: true
+    }, function (req, email, password, done) {
+        user.findOne({'email': email}, function (err, member) {
+            if (err) {
+                return done(err);
+            }
+            if (member) {
+                return done(null, false);
+            } else {
+                var newPlayer = new user();
+                newPlayer.email = email;
+                newPlayer.password = newPlayer.createPasswordHash(password);
+                newPlayer.save(function (err) {
+                    if (err) {
+                        throw err;
+                    }
+                    return done(null, newPlayer);
+                });
+            }
         });
+    }));
 
-        passport.use('signup-strategy', new LocalStrategy({
-            usernameField: 'email',
-            passwordField: 'password',
-            passReqToCallback: true
-        }, function (req, email, password, done) {
-            user.findOne({'email': email}, function (err, member) {
-                if (err) {
-                    return done(err);
-                }
-                if (member) {
-                    return done(null, false);
-                } else {
-                    var newPlayer = new user();
-                    newPlayer.email = email;
-                    newPlayer.password = newPlayer.createPasswordHash(password);
-                    newPlayer.save(function (err) {
-                        if (err) {
-                            throw err;
-                        }
-                        return done(null, newPlayer);
-                    });
-                }
-            });
-        }));
-
-        passport.use('login-strategy', new LocalStrategy({
-            usernameField: 'email',
-            passwordField: 'password',
-            passReqToCallback: true
-        }, function (req, email, password, done) {
-            console.log("inside authentication");
-            user.findOne({'email': email}, function (err, member) {
-                if (err) {
-                    return done(err);
-                }
-                if (!member) {
-                    return done(null, false);
-                }
-                if (!member.comparePassword(password)) {
-                    return done(null, false);
-                }
-                return done(null, member);
-            });
-        }));
-
-    };
+    passport.use('login-strategy', new LocalStrategy({
+        usernameField: 'email',
+        passwordField: 'password',
+        passReqToCallback: true
+    }, function (req, email, password, done) {
+        user.findOne({'email': email}, function (err, member) {
+            if (err) {
+                return done(err);
+            }
+            if (!member) {
+                return done(null, false);
+            }
+            if (!member.comparePassword(password)) {
+                return done(null, false);
+            }
+            return done(null, member);
+        });
+    }));
 
 
     /*----Authentication code ends here----*/
 
-}
+};
