d589968634da1355803253ed193d983024505d09
Rabbia Umer <rabbiajahangir92@gmail.com>
2016-08-03 22:15:18 -0400

    improvement(avatar): adds the links to images as a JSON object

---
+++ configs/avatar.js
@@ -0,0 +1,81 @@
+/**
+ * Created by rabbiaumer on 8/3/16.
+ */
+
+module.exports = {
+    av_01: {
+        link: 'img/actor.png'
+    },
+    av_02: {
+        link: 'img/actress.png'
+    },
+    av_03: {
+        link: 'img/astronaut.png'
+    },
+    av_04: {
+        link: 'img/basketball_man.png'
+    },
+    av_05: {
+        link: 'Bellboy_2.png'
+    },
+    av_06: {
+        link: 'img/Bellboy.png'
+    },
+    av_07: {
+        link: 'img/boxer_hispanic.png'
+    },
+    av_08: {
+        link: 'img/bride_hispanic.png'
+    },
+    av_09: {
+        link: 'img/cook2_man.png'
+    },
+    av_10: {
+        link: 'img/cook2_woman_red_head_material.png'
+    },
+    av_11: {
+        link: 'img/cool_man.png'
+    },
+    av_12: {
+        link: 'img/dead.png'
+    },
+    av_13: {
+        link: 'img/diver.png'
+    },
+    av_14: {
+        link: 'img/doctor_Female_Hispanic_material.png'
+    },
+    av_15: {
+        link: 'img/driver.png'
+    },
+    av_16: {
+        link: 'img/engineering_woman_blonde_material.png'
+    },
+    av_16: {
+        link: 'img/engineering2.png'
+    },
+    av_16: {
+        link: 'img/football_man.png'
+    },
+    av_16: {
+        link: 'img/Harry_Potter_hufflepuff.png'
+    },
+    av_16: {
+        link: 'img/hittler.png'
+    },
+    av_16: {
+        link: 'img/japanese.png'
+    },
+    av_17: {
+        link: 'img/man_hispanic.png'
+    },
+    av_18: {
+        link: 'img/matrix_agent_smith_material.png'
+    },
+    av_19: {
+        link: 'img/matrix_trinity_material.png'
+    },
+    av_20: {
+        link: 'img/paramedic_woman_hispanic_material.png'
+    }
+}
