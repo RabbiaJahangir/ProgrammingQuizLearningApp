e52bd812b0c0d19b7bd054e2053ef7d0e43b9859
Rabbia Umer <rabbiajahangir92@gmail.com>
2016-11-19 12:17:36 -0500

    chore(categories-route): some last work that I don't remember

--- routes/categories-route.js
+++ routes/categories-route.js
@@ -7,7 +7,7 @@ module.exports = function (app, mongoose) {
     var cat = require('../models/categories')(mongoose);
 
     app.get('/categories', function (req, res) {
-        cat.find({}, 'name-_id', function (err, cats) {
+        cat.find({}, function (err, cats) {
             if (!err) {
                 res.send(cats);
             }
