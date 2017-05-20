36e53bd444b36cfe01773fdbe9317526b5fc81cc
Rabbia Umer <rabbiajahangir92@gmail.com>
2017-05-20 08:37:26 -0400

    restructure the credentials file

--- credentials.js
+++ credentials.js
@@ -3,11 +3,7 @@
  */
 
 module.exports = {
-    cookie: {
-        secretForCookie: 'Rabbia Jahangir Final Year Project',
-        cookieAge: 2592000000
-    },
-    mongo: {
-        dbConnectionString: "mongodb://rabbiaumer:inkpot9090@ds011459.mlab.com:11459/quizr"
-    }
+  secret: 'Rabbia Jahangir Final Year Project',
+  dbConnectionString: "mongodb://rabbiaumer:inkpot9090@ds011459.mlab.com:11459/quizr"
+
 }
