67f234755ae8f06ef36f0f39d685a7150fe4c216
Rabbia Umer <rabbiajahangir92@gmail.com>
2017-05-20 08:37:04 -0400

    remove all of the passport.js related code for authentication

--- authentication/authenticate.js
+++ authentication/authenticate.js
@@ -6,67 +6,6 @@ module.exports = function (passport, LocalStrategy, mongoose) {
 
   var user = require("../models/user")(mongoose);
 
-  /*----Authentication code starts here----*/
-
-  passport.serializeUser(function (user, done) {
-    done(null, user.id);
-  });
-
-  passport.deserializeUser(function (id, done) {
-
-    user.findById(id, function (err, user) {
-      done(err, user);
-    });
-
-  });
-
-  passport.use('signup-strategy', new LocalStrategy({
-    usernameField: 'email',
-    passwordField: 'password',
-    passReqToCallback: true
-  }, function (req, email, password, done) {
-
-    user.findOne({'email': email}, function (err, member) {
-      if (err) {
-        return done(err);
-      }
-      if (member) {
-        return done(null, false);
-      } else {
-        var newPlayer = new user();
-        newPlayer.email = email;
-        newPlayer.password = newPlayer.createPasswordHash(password);
-        newPlayer.save(function (err) {
-          if (err) {
-            throw err;
-          }
-          return done(null, newPlayer);
-        });
-      }
-    });
-
-  }));
-
-  passport.use('login-strategy', new LocalStrategy({
-    usernameField: 'email',
-    passwordField: 'password',
-    passReqToCallback: true
-  }, function (req, email, password, done) {
-
-    user.findOne({'email': email}, function (err, member) {
-      if (err) {
-        return done(err);
-      }
-      if (!member) {
-        return done(null, false);
-      }
-      if (!member.comparePassword(password)) {
-        return done(null, false);
-      }
-      return done(null, member);
-    });
-
-  }));
 
   /*----Authentication code ends here----*/
 
