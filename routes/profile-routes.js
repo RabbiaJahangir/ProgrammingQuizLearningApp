7979f587c59bf08a5011872efafb9ee0efc375b0
Rabbia Umer <rabbiajahangir92@gmail.com>
2017-07-19 00:24:48 -0400

    make avatar routes public again, logic for  returning user's avata in user's profile

--- routes/profile-routes.js
+++ routes/profile-routes.js
@@ -2,22 +2,26 @@
  * Created by rabbiaumer
  */
 
+
 module.exports = function (app, mongoose, jwt) {
+
+  var avatarRefs = require('../configs/avatar');
 
   // var profile = require('./../models/user')(mongoose);
 
   app.get('/user-profile', function (req, res) {
     // check header or url parameters or post parameters for token
     var token = req.body.token || req.query.token || req.headers['x-access-token'];
-
     var decodedToken = jwt.decode(token);
+    var host = req.headers.host;
+
     var userInfo = {
       email: decodedToken._doc.email,
       firstName: decodedToken._doc.firstName,
       lastName: decodedToken._doc.lastName,
       level: decodedToken._doc.level,
-      avatar: "http://" + req.headers.host + decodedToken._doc.avatar;
-  }
+      avatar: "http://" + host + "/" + avatarRefs[decodedToken._doc.avatar].link
+    }
 
 
     res.send(userInfo);
