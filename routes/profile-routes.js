b510377dd2922afe4c2879c6e0ff91fdc016966c
Rabbia Umer <rabbiajahangir92@gmail.com>
2017-07-30 04:15:58 -0400

    new route for setting avatar

--- routes/profile-routes.js
+++ routes/profile-routes.js
@@ -5,7 +5,8 @@
 
 module.exports = function (app, mongoose, jwt) {
 
-  var helper = require('../helper');
+  var helper = require('../helper'),
+    User = require('../models/user');
 
   // var profile = require('./../models/user')(mongoose);
 
@@ -24,6 +25,32 @@ module.exports = function (app, mongoose, jwt) {
     };
 
     res.send(userInfo);
+  });
+
+  app.post('/set-avatar', function (req, res) {
+
+    // check header or url parameters or post parameters for token
+    var token = req.body.token || req.query.token || req.headers['x-access-token'];
+    var decodedToken = jwt.decode(token);
+
+    // var host = req.headers.host;
+    // var avatarLinks = [];
+    // for (var avatar in avatarRefs) {
+    //   avatarLinks.push("http://" + host + "/" + avatarRefs[avatar].link);
+    // }
+
+    User.findOneAndUpdate({'email': decodedToken._doc.email}, {'avatar': req.avatarName}, {upsert: true}, function (err, doc) {
+      if (err)
+        return res.send({
+          success: false,
+          message: "failed"
+        });
+
+      res.send({
+        success: true,
+        message: "success"
+      });
+    });
   });
 
 };
