7ac5e9c3011e7a0e5520b63d2d4ebf85d2c4427e
Rabbia Umer <rabbiajahangir92@gmail.com>
2017-08-27 03:09:30 -0400

    new route to send questions for the competition

--- routes/competition-routes.js
+++ routes/competition-routes.js
@@ -1,7 +1,29 @@
-module.exports = function (app) {
+module.exports = function (app, user, questions) {
 
+  var User = user;
+  var Questions = questions;
 
+  // This route requires that Front End sends the categoryId as a parameter
   app.get('/questions', function (req, res) {
-    res.send(req.decoded);
+    var userEmail = req.decoded._doc.email;
+    var query = req.query;
+
+    // Finding record for the current user in DB, to get the current level of the user/player
+    User.findOne({email: userEmail}, function (err, user) {
+      if (!err) {
+
+        // Find the questions by the user level and the category he has selected
+        Questions.find({category: query.categoryId, level: (user.level + 1)}, function (err, questions) {
+          if (!err) {
+            res.send(questions);
+          } else {
+            console.log(err);
+          }
+        });
+
+      } else {
+        console.log(err);
+      }
+    });
   });
 };
