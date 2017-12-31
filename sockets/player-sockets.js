b68b9bdae49cd4f36e3cb2a7ca6a6c890b2da532
Rabbia Umer <rabbiajahangir92@gmail.com>
2017-12-31 13:03:47 -0500

    all the changes made that I don't remeber

--- sockets/player-sockets.js
+++ sockets/player-sockets.js
@@ -1,6 +1,10 @@
+var util = require('util')
+
 module.exports = function (io, socketioJwt, credentials) {
 
-// to authorize the socket connections
+  var ALL_PLAYERS_ROOM = 'allPlayers'; // name for room of all players
+
+  // to authorize the socket connections
   io.set('authorization', socketioJwt.authorize({
     secret: credentials.secret,
     handshake: true
@@ -11,16 +15,31 @@ module.exports = function (io, socketioJwt, credentials) {
    */
 
   io.on('connect', function (player) {
+
+    console.log('hello! ', player.request.decoded_token);
+
     // player has been connected, now let him join allPlayers room
 
-    player.join('allPlayers', function () {
+    player.join(ALL_PLAYERS_ROOM, function () {
       // emit playersRoomJoined event
-      player.emit('playersRoomJoined', {})
-    })
+      // player.emit('playersRoomJoined');
+
+      console.log(player.rooms);
+      console.log(player.id);
+
+      // player.emit('totalPlayers', {totalPlayers:Object.})
+      // console.log(io.sockets.clients(ALL_PLAYERS_ROOM));
+      console.log(io.sockets.adapter.rooms[ALL_PLAYERS_ROOM].length);
+    });
+
+    console.log('player connected');
+
+    player.emit('connectedObj', util.inspect(player));
 
     player.on('disconnect', function (player) {
       console.log('player disconnected');
     });
+
   });
 
 };
