939eea7367f4c1b97ab212c1587e1eb0352be696
Rabbia Umer <rabbiajahangir92@gmail.com>
2018-02-04 13:56:50 -0500

    fixed couple of things

--- routes/competition-routes.js
+++ routes/competition-routes.js
@@ -6,16 +6,16 @@ module.exports = function (app, user, questions, cat) {
 
   // This route requires that Front End sends the categoryId as a parameter
   app.get('/questions', function (req, res) {
-    var query = req.query;
+    var categoryId = req.query.categoryId;
 
     var categoryLevel = req.user.levels.find(function (levelObj) {
-      levelObj.category._id === query.categoryId;
+      return levelObj.category._id === categoryId;
     });
 
     var userCategoryLevel = categoryLevel && categoryLevel.level ? categoryLevel.level : 1;
 
     // Find the questions by the user level and the category he has selected
-    Questions.find({category: query.categoryId, level: userCategoryLevel}, function (err, questions) {
+    Questions.find({category: categoryId, level: userCategoryLevel}, function (err, questions) {
       if (!err) {
         res.send(questions);
       } else {
